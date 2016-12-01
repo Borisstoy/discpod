5d65079fb49ebf1c76aca88269ed8e8b34063f42
BorisstoyE <boris.stoy@gmaiL.com>
2016-12-01 11:15:55 +0100

    Set up dashboard

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,5 +1,7 @@
 class PodcastsController < ApplicationController
-	
+	before_action :find_podcast, only: [:show, :dashboard]
+	before_action :find_episode, only: [:show, :dashboard]
+
 	# for index, we need to loop through all episodes, using podcastS
 	def index
 		@podcasts = Podcast.all.order('created_at desc')
@@ -7,8 +9,29 @@ class PodcastsController < ApplicationController
 
 	# for the show method, the use of singular sends us back to show 1 podcast
 	def show
-		@podcast = Podcast.find(params[:id])
+		# @podcast = Podcast.find(params[:id])
 		# need to find the episode as well
+		# @episodes = Episode.where(podcast_id: @podcast).order("created_at desc")
+	end
+
+	def dashboard
+		# if signed in, we want users to go to dashboard
+		# so we need to authenticate users
+		# see routes
+	end
+
+	private
+
+	def find_episode
 		@episodes = Episode.where(podcast_id: @podcast).order("created_at desc")
+	end
+
+	def find_podcast
+		# if rails can't find a podcast
+		if params[:id].nil?
+			@podcast = current_podcast
+		else
+		@podcast = Podcast.find(params[:id])
+		end
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
   	devise_for :podcasts
-	root 'welcome#index'
+	# root 'welcome#index'
+	# moved after authentification
 
 	resources :podcasts, only: [:index, :show] do
 	# don't forget "do"
 	# create route for episodes; an episode belongs to a podcat, so it's nested under it.
 		resources :episodes
 	end
+
+	# for rerouting signed in users
+	authenticated :podcast do
+		root 'podcasts#dashboard', as: "authenticated_root"
+	end
+
+	root 'welcome#index'
 end
